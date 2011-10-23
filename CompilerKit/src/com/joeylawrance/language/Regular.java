4b37b9487d8d0d783d9cff02df364b46932fe4a7
Joseph Lawrance <lawrancej@wit.edu>
2011-10-23 01:35:26 -0400

    Need replacement for reflection so Regular and ContextFree cooperate.

--- CompilerKit/src/com/joeylawrance/language/Regular.java
+++ CompilerKit/src/com/joeylawrance/language/Regular.java
@@ -10,7 +10,7 @@ public class Regular {
 	/**
 	 * TODO: implement group capture, forward/backward references, tokens, scanning
 	 * TODO: boolean operations on regexes (e.g., not, and)
-	 * 
+	 * TODO: need to find an alternative to reflection. I think some sort of locator using instanceof would do wonders.
 	 */
 	/**
 	 * Abstract superclass for all parser combinator classes.
