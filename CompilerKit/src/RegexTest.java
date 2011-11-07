6ed6065f8199454540d471747854a1642b6dd7d1
Joseph Lawrance <lawrancej@wit.edu>
2011-11-07 16:11:51 -0500

    Okay, looks like performance is improved 10-fold. :-)

--- CompilerKit/src/RegexTest.java
+++ CompilerKit/src/RegexTest.java
@@ -5,12 +5,12 @@ import com.joeylawrance.language.Parser;
 public class RegexTest {
 
 	public static void main(String[] args) {
-		long before = System.currentTimeMillis();
 		Parser r = catenation(times(alpha(),3,8), optional(digit()), string("@bridgew.edu"));
+		long before = System.currentTimeMillis();
 		System.out.println(r);
 		System.out.println(r.recognize("somebody@bridgew.edu"));
 		System.out.println(r.recognize("somebody@wit.edu"));
-		System.out.println(System.currentTimeMillis() - before); // 183ms is bad
+		System.out.println(System.currentTimeMillis() - before); // 12ms is better than before
 		r = catenation(not(symbol('\n')), string("\n"));
 		System.out.println(r);
 		System.out.println(r.recognize("abc\n\n"));
