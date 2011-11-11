09c452fb149f8cb0d099b982318f83e1771e165f
Joseph Lawrance <lawrancej@wit.edu>
2011-11-11 09:05:05 -0500

    Updated parser interface with tagging visitable interface

--- CompilerKit/src/com/joeylawrance/language/Parser.java
+++ CompilerKit/src/com/joeylawrance/language/Parser.java
@@ -1,9 +1,11 @@
 package com.joeylawrance.language;
 
+import com.joeylawrance.visitor.Visitable;
+
 // TODO: Allow recognize to work on abitrary streams? make generic? this would also affect derivative visitor
 // TODO: move node classes into own package and use public interfaces everywhere
 /**
  * Abstract superclass for all parser combinator classes.
  * Uses the Composite and Visitor design patterns.
  */
-public interface Parser {}
+public interface Parser extends Visitable {}
